b6db62849c588103057a4f218748ade1bd3884a9
ShamalaManivannan <76401128+ShamalaManivannan@users.noreply.github.com>
2025-07-14 17:40:26 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import plotly.express as px
+import plotly.graph_objects as go
+
+data = pd.read_csv("WHO-COVID-19-global-data.csv") 
+
+data.columns = ("Date_reported","Country_code","Country","WHO_region","New_cases","Cumulative_cases","New_deaths","Cumulative_deaths")
+
+print(type(data["Date_reported"][0]))
+
+data["Date_reported"] = pd.to_datetime(data["Date_reported"])
+
+print(type(data["Date_reported"][0]))
+
+data_dates = data.groupby("Date_reported").sum()
+print(data_dates)
+
+fig1 = go.Figure()
+
+fig1.add_trace(go.Scatter(x = data_dates.index , y = data_dates["Cumulative_cases"], fill = "tonexty" , line_color = "blue"))
+fig1.update_layout(title = "Cumulative Cases Worldwide")
+fig1.write_html("Fig1.html", auto_open = True)
+
+fig2 = go.Figure()
+
+fig2.add_trace(go.Scatter(x = data_dates.index ,  y = data_dates["New_cases"], fill = "tonexty", line_color = "red"))
+fig2.update_layout(title = "New cases Worldwide")
+fig2.write_html("Fig2.html", auto_open = True)
+
+fig3 = go.Figure()
+
+fig3.add_trace(go.Scatter(x = data_dates.index , y = data_dates["Cumulative_deaths"], fill = "tonexty", line_color = "green"))
+fig3.update_layout(title = "Cumulative Deaths Worldwide")
+fig3.write_html("Fig3.html", auto_open = True)
+
+fig4 = go.Figure()
+
+fig4.add_trace(go.Scatter( x = data_dates.index, y = data_dates["New_deaths"], fill = "tonexty", line_color = "orange"))
+fig4.update_layout(title = "New deaths Worldwide")
+fig4.write_html("Fig4.html", auto_open = True)
